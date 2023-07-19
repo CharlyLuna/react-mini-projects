70dc56781383e607e2ff4f9ec8ac5174daf2eecb
CharlyLuna <cluna10@ucol.mx>
2023-07-18 20:19:37 -0600

    Remove empty lines and unused file

--- projects/advice-generator/src/App.jsx
+++ projects/advice-generator/src/App.jsx
@@ -13,11 +13,8 @@ export const App = () => {
         <div className='absolute top-1/2 -translate-y-1/2 flex flex-col justify-center w-full max-w-md md:max-w-lg gap-6 text-center bg-dark-grayish-blue m-6 py-10 rounded-md'>
           <h1 className='text-primary-green'>A D V I C E # {id}</h1>
           <p className='text-2xl px-6 md:px-12 text-primary-cyan'>{isLoading ? '...' : `“${advice}”`}</p>
-          <img
-            className='px-6 md:hidden pb-8' src={DividerMobile} alt=''
-          />
+          <img className='px-6 md:hidden pb-8' src={DividerMobile} alt='' />
           <img className='px-10 hidden md:block pb-8' src={DividerDesktop} alt='' />
-
           <button onClick={getNewRandomAdvice} className='dice-button'>
             <img className='h-7 w-auto' src={DiceIcon} alt='dice icon' />
           </button>
